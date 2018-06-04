2897df25790769d6489f30a7ed460af28406b19d
ignitz <ignitzhjfk@gmail.com>
2018-06-04 01:05:21 -0300

    redux with async actions

--- src/store.js
+++ src/store.js
@@ -1,5 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import thunk from "redux-thunk";
+import promise from "redux-promise-middleware";
 
 import mathReducer from "./reducers/mathReducer";
 import userReducer from "./reducers/userReducer";
@@ -7,5 +9,5 @@ import userReducer from "./reducers/userReducer";
 export default createStore(
   combineReducers({ math: mathReducer, user: userReducer }),
   {},
-  applyMiddleware(logger)
+  applyMiddleware(logger, thunk, promise())
 );
